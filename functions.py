0dac677e0161ec6865410942452f1c8c7c30d5db
mukafries <blessing201880@outlook.com>
2025-11-29 12:31:59 +0100

    messed around with task 4 scopes

--- functions.py
+++ functions.py
@@ -29,13 +29,30 @@ print("======================================")
 
 #Task 4
 school_name = "Chris-Ade International Academy"
+test_number = 102
 def print_school():
+  print(f"Test number - {test_number}")
+  # print(f"School name - {school_name}")
+  print("My question- Why does test_number print but school_name does not print? The type?")
+  print("===========")
   teacher_name = "Mr. Esho" #This is a local variable
   print(f"This is from the local local scope: {teacher_name}")
 
   #overriding the global variable school_name overridden within the local scope
   school_name ="Local Chris-Ade International Academy" #this is also a local variable
-  print(f"This is the overridden school_name from the local function: ", school_name)
+  print("This is the overridden school_name from the local function: ", school_name)
+
+  def class_room():
+      print(f"Can {teacher_name} print in tis scope? \n If no error, that means it did. Although the school name without overriding in the print_school function did not print")
+
+      class_room_name = "JSS 3"
+      # print_school(f"{class_room} of {school_name}")
+      #Encountered an error with the above code because school_name was not accessible in the scope
+  
+  #print(class_room_name)
+  #above gave me an error also, did not print
+
+  class_room()
 
 print_school()
 print("=============")
